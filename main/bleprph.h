8fdb6293ffd99407b3052673bc0231947c5c2941
Mahmood_Reda <mr4141705@gmail.com>
2024-03-07 17:00:01 +0300

    organize ESP Code

--- main/bleprph.h
+++ main/bleprph.h
@@ -20,6 +20,23 @@
 #ifndef H_BLEPRPH_
 #define H_BLEPRPH_
 
+
+#include "esp_nimble_hci.h"
+#include "nimble/nimble_port.h"
+#include "nimble/nimble_port_freertos.h"
+#include "host/ble_hs.h"
+#include "host/util/util.h"
+#include "console/console.h"
+#include "services/gap/ble_svc_gap.h"
+#include "esp_bt.h"
+#include "blecent.h"
+#include "bleprph.h"
+
+#include "driver/uart.h"
+#include "driver/gpio.h"
+#include "driver/spi_slave.h"
+#include "freertos/task.h"
+#include "connection_driver.h"
 #include <stdbool.h>
 #include "nimble/ble.h"
 #include "modlog/modlog.h"
@@ -41,6 +58,8 @@ struct ble_gatt_register_ctxt;
 void gatt_svr_register_cb(struct ble_gatt_register_ctxt *ctxt, void *arg);
 int gatt_svr_init(void);
 
+
+void BLEServer(char* Servername);
 /* Console */
 int scli_init(void);
 int scli_receive_key(int *key);
