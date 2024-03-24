9d5c84e8523afc31a347a41d14238ba8b40d90ef
Mahmood_Reda <mr4141705@gmail.com>
2024-03-24 13:16:36 +0300

    Editing uart_config  (baud_rate from 115200  to 921600 )

--- main/main.c
+++ main/main.c
@@ -103,7 +103,7 @@ static void ble_uart_init(void) {
 
 	/* Configure parameters of an UART driver,
 	 * communication pins and install the driver */
-	uart_config_t uart_config = { .baud_rate = 115200, .data_bits =
+	uart_config_t uart_config = { .baud_rate = 921600, .data_bits =
 			UART_DATA_8_BITS, .parity = UART_PARITY_DISABLE, .stop_bits =
 			UART_STOP_BITS_1, .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
 			.source_clk = UART_SCLK_DEFAULT, };
